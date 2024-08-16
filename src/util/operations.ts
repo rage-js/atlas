a63bd0723ebd7af9d650f3ee1ad7025e077147ea
Maghish <mail4mithran@gmail.com>
2024-08-16 14:15:36 +0530

    Working on pushLocalDatabase

--- src/util/operations.ts
+++ src/util/operations.ts
@@ -4,6 +4,9 @@ import { RageConfigurations } from "../main";
 import { MongoClient } from "mongodb";
 import writeJsonFiles from "./writeJsonFiles";
 import sleep from "./sleep";
+import path from "path";
+import * as fsS from "fs";
+import * as fsP from "fs/promises";
 
 async function pullCloudDatabase(
   configurations: RageConfigurations,
@@ -158,7 +161,75 @@ async function pushLocalDatabase(
         configurations.databaseSpecificSettings.secretKey;
     }
 
-    console.log(newConfigSettings);
+    // Push the local database to the cloud
+
+    // Create a MongoDB connection
+    const mongodbInstance = new MongoClient(newConfigSettings.secretKey!);
+    await mongodbInstance.connect();
+
+    const actualPath = path.join(process.cwd(), databasePath);
+
+    newConfigSettings.dbs!.forEach(async (dbName) => {
+      const db = mongodbInstance.db(dbName);
+
+      let allCollections = await fsP.readdir(
+        path.join(actualPath, `/${dbName}`),
+        "utf-8"
+      );
+
+      allCollections = allCollections.filter(
+        (file) => path.extname(file).toLowerCase() === ".json"
+      );
+
+      allCollections.forEach(async (collection) => {
+        collection = collection.slice(0, -5);
+        const cols = await db.listCollections({ name: collection }).toArray();
+        if (cols.length === 0) {
+          await db.createCollection(collection);
+          // console log on collection creation
+        }
+      });
+
+      const collections = await db.listCollections().toArray();
+      for (const collection of collections) {
+        const collectionName = collection.name;
+        if (collectionName in newConfigSettings.excludeCollections!) {
+          // Skip
+        } else {
+          const fullPath = path.join(
+            actualPath,
+            `/${dbName}`,
+            `/${collectionName}.json`
+          );
+
+          let content: any;
+
+          try {
+            content = await fsP.readFile(fullPath, "utf-8");
+            if (content === "[]") {
+              content = [];
+            } else {
+              content = JSON.parse(content);
+            }
+
+            // Empty the collection
+            const c = db.collection(collectionName);
+            await c.deleteMany({});
+
+            content.forEach(async (document: any) => {
+              c.insertOne(document).catch((error: any) => {
+                console.log(`Unexpected error occurred!`);
+              });
+            });
+
+            continue;
+          } catch (error: any) {
+            console.log(`Unexpected error occurred!`);
+          }
+        }
+      }
+    });
+
     await sleep(10000);
   } catch (error: any) {
     if (error.message === "ExitPromptError") {
